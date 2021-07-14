c08e44cb9c723912f7445483febac3c00dfcd5ba
gayathiri24102009 <78957848+gayathiri24102009@users.noreply.github.com>
2021-07-14 17:30:19 +0530

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,9 @@
+function back(){
+window.location="activity_1.HTML";
+
+}
+
+function getScore(){
+    score=localStorage.getItem("score");
+    document.getElementById("update").innerHTML="<h1>score: "+score+"</h1>"
+}
